2a302d1b247209ca57e40f9337b72d66049eaedf
david <david.mlcoch@gmail.com>
2024-10-26 17:35:14 +0100

    update divide_supervisor

--- entropy_labs/src/entropy_labs/supervision/supervisors.py
+++ entropy_labs/src/entropy_labs/supervision/supervisors.py
@@ -1,4 +1,4 @@
-from typing import Callable, Any
+from typing import Callable
 from .config import SupervisionDecision, SupervisionDecisionType
 
 def divide_supervisor(min_a: int=0, min_b: int = 0):
@@ -12,7 +12,7 @@ def divide_supervisor(min_a: int=0, min_b: int = 0):
     Returns:
         Callable: A supervisor function that checks the arguments against the specified minimums.
     """
-    def supervisor(func: Callable, a: float, b: float) -> SupervisionDecision:
+    def supervisor(func: Callable, a: float, b: float, **kwargs) -> SupervisionDecision:
         """Supervisor that checks if both numbers are above the specified minimums."""
         if a > min_a and b > min_b:
             return SupervisionDecision(
