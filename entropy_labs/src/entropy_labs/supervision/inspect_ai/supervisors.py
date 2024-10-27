85fc1025bbb60ffc6ce890b748d5afcbc7d74b4a
david <david.mlcoch@gmail.com>
2024-10-27 12:38:37 +0000

    create Supervisor + llm_supervisor

--- entropy_labs/src/entropy_labs/supervision/inspect_ai/supervisors.py
+++ entropy_labs/src/entropy_labs/supervision/inspect_ai/supervisors.py
@@ -3,12 +3,7 @@ from typing import List, Dict, Optional, Set
 from inspect_ai.approval import Approval, Approver, approver
 from inspect_ai.solver import TaskState
 from inspect_ai.tool import ToolCall, ToolCallView
-from entropy_labs.supervision.common import check_bash_command, check_python_code, human_supervisor_wrapper
-from entropy_labs.supervision.inspect_ai.utils import (
-    transform_entropy_labs_approval_to_inspect_ai_approval
-)
-from entropy_labs.api._supervision import get_llm_supervision_decision_api
-from ._config import DEFAULT_TIMEOUT, DEFAULT_SUGGESTIONS
+from inspect_ai._util.registry import registry_lookup
 
 @approver
 def bash_approver(
@@ -22,6 +17,7 @@ def bash_approver(
         view: ToolCallView,
         state: Optional[TaskState] = None,
     ) -> Approval:
+        from entropy_labs.supervision.common import check_bash_command
         command = str(next(iter(call.arguments.values()))).strip()
         is_approved, explanation = check_bash_command(command, allowed_commands, allow_sudo, command_specific_rules)
         
@@ -45,6 +41,7 @@ def python_approver(
         view: ToolCallView,
         state: Optional[TaskState] = None,
     ) -> Approval:
+        from entropy_labs.supervision.common import check_python_code
         code = str(next(iter(call.arguments.values()))).strip()
         is_approved, explanation = check_python_code(
             code, 
@@ -62,25 +59,27 @@ def python_approver(
     return approve
 
 @approver
-def human_approver(agent_id: str, approval_api_endpoint: Optional[str] = None, n: int = DEFAULT_SUGGESTIONS, timeout: int = DEFAULT_TIMEOUT) -> Approver:
+def human_approver(agent_id: str, approval_api_endpoint: Optional[str] = None, n: int = 3, timeout: int = 300) -> Approver:
     async def approve(
         message: str,
         call: ToolCall,
         view: ToolCallView,
         state: Optional[TaskState] = None,
     ) -> Approval:
+        from entropy_labs.supervision.common import human_supervisor_wrapper        
+        from entropy_labs.supervision.common import _transform_entropy_labs_approval_to_inspect_ai_approval
         if state is None:
             return Approval(decision="escalate", explanation="TaskState is required for this approver.")
 
         logging.info(f"Generating {n} tool call suggestions for user review")
 
         approval_decision = await human_supervisor_wrapper(task_state=state, call=call, backend_api_endpoint=approval_api_endpoint, agent_id=agent_id, timeout=timeout, use_inspect_ai=True, n=n)
-        inspect_approval = transform_entropy_labs_approval_to_inspect_ai_approval(approval_decision)
+        inspect_approval = _transform_entropy_labs_approval_to_inspect_ai_approval(approval_decision)
         return inspect_approval
     return approve
 
 @approver
-def llm_approver(approval_api_endpoint: str, agent_id: str, n: int = DEFAULT_SUGGESTIONS, timeout: int = DEFAULT_TIMEOUT) -> Approver:
+def llm_approver(instructions: str, openai_model: str, system_prompt: str, include_context: bool, agent_id: Optional[str] = None) -> Approver:
     async def approve(
         message: str,
         call: ToolCall,
@@ -89,8 +88,17 @@ def llm_approver(approval_api_endpoint: str, agent_id: str, n: int = DEFAULT_SUG
     ) -> Approval:
         if state is None:
             return Approval(decision="escalate", explanation="TaskState is required for this approver.")
+        from entropy_labs.supervision.supervisors import llm_supervisor
+        from entropy_labs.supervision.config import supervision_config
+        from entropy_labs.supervision.common import _transform_entropy_labs_approval_to_inspect_ai_approval
 
-        approval_decision = await get_llm_supervision_decision_api(task_state=state, call=call, backend_api_endpoint=approval_api_endpoint, agent_id=agent_id, timeout=timeout)
-        inspect_approval = transform_entropy_labs_approval_to_inspect_ai_approval(approval_decision)
-        return inspect_approval
+        llm_supervisor_func = llm_supervisor(instructions=instructions, openai_model=openai_model, system_prompt=system_prompt, include_context=include_context)
+        
+        function_name = call.function 
+        func = registry_lookup('tool', function_name)
+        supervision_config.context.inspect_ai_state = state
+        approval_decision = llm_supervisor_func(func=func, supervision_context=supervision_config.context,
+                                               **call.arguments)
+        inspect_approval = _transform_entropy_labs_approval_to_inspect_ai_approval(approval_decision)
+        return inspect_approval      
     return approve
