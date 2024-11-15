38f3c5d0feff1d1aaed255488235553f3afbcb6f
Joe <joehewett1@gmail.com>
2024-11-15 11:31:54 -0800

    remove tau-bench

--- tau-bench/tau_bench/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright Sierra
-
-from tau_bench.envs.base import Env as Env
-from tau_bench.agents.base import Agent as Agent
--- tau-bench/tau_bench/envs/retail/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright Sierra
-
-from tau_bench.envs.retail.env import MockRetailDomainEnv as MockRetailDomainEnv
