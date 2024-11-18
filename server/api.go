cb3271f33c49066fe33228281804396362c3d008
Joe <joehewett1@gmail.com>
2024-11-18 04:10:03 -0800

    specify allowed API methods

--- server/api.go
+++ server/api.go
@@ -228,7 +228,7 @@ func enableCorsMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		// Handle OPTIONS requests
